3ea9dae2a12bd317966d65649820dadae258ecb3
Melu <melacoton11@gmail.com>
2023-10-19 11:50:17 -0300

    El commiteado

--- src/Navegador.js
+++ src/Navegador.js
@@ -1,17 +1,21 @@
 import React from "react";
 import {BrowserRouter, Link} from 'react-router-dom';
 
+const NewPost = ({ NewPost }) =>{
+  return <button className="btnsucces" onClick ={() => NewPost++}> {NewPost} New Post!!</button>
+ };
+
 function Navegador() {
 return (
 <BrowserRouter>
   <nav className="dos">
    <ul>
-    <li className="boton"><Link to="/inicio" >Home</Link></li>
+    <li className="boton"><Link to="/Home" >Home</Link></li>
     <li className="boton"><Link to="/admin">Admin</Link></li>
-    <li><Link to="/NewPost"><button href="#"className="btnsucces">New Post!!</button></Link></li>
+    <NewPost />
     </ul>
  </nav>
 </BrowserRouter>
 );
 }
-export default Navegador;
+export default Navegador; 
